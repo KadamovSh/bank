a4e7ee266a9db1d12fedcb519a1dcc2c854e56f4
MrShoir <kadamov.shoir24@mail.ru>
2026-02-15 14:19:35 +0500

    Добавлена функция Withdraw (снятие деньги)

--- pkg/bank/card/card.go
+++ pkg/bank/card/card.go
@@ -18,15 +18,20 @@ func Total(cards []types.Card) types.Money {
     return sum
 }
 
-// Issue создаёт экземпляр карты с предопределёнными полями
-func Issue(currency types.Currency, color string, name string) types.Card {
-    return types.Card{
-        ID:        1000,
-        PAN:       "5058 xxxx xxxx 0001",
-        Balance:   0,
-        Currency:  currency,
-        Color:     color,
-        Name:      name,
-        Active:    true,
+// Withdraw снимает деньги с карты.
+func Withdraw(card *types.Card, amount types.Money) {
+    const withdrawLimit = 20_000_00
+    if amount < 0 {
+        return
     }
+    if amount > withdrawLimit {
+        return
+    }
+    if !card.Active {
+        return
+    }
+    if card.Balance < amount {
+        return
+    }
+    card.Balance -= amount
 }
