1741c202c68e3fce089a2a86cf3fbd195c8f6d2e
MrShoir <kadamov.shoir24@mail.ru>
2026-02-15 13:19:53 +0500

    Добавлены типы

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,28 @@
+package types
+
+
+
+type Money int64
+
+//Currency представляет код валюты
+type Currency string
+
+//Коды валют
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+// PAN представляет номер карты
+type PAN string
+
+//Card представляет информацию о платежной карте
+type Card struct {
+	ID      	int
+	PAN     	PAN
+	Balance 	Money
+	Currency 	Currency
+	Color 		string
+	Name 		string 
+	Active 		bool
+}
