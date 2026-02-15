afc64911a61bd51ee436344865c349a7d5ff5e96
MrShoir <kadamov.shoir24@mail.ru>
2026-02-15 14:09:51 +0500

    Добавлен функция Issue (выпуск карты)

--- pkg/bank/card/card.go
+++ pkg/bank/card/card.go
@@ -17,3 +17,16 @@ func Total(cards []types.Card) types.Money {
     }
     return sum
 }
+
+// Issue создаёт экземпляр карты с предопределёнными полями
+func Issue(currency types.Currency, color string, name string) types.Card {
+    return types.Card{
+        ID:        1000,
+        PAN:       "5058 xxxx xxxx 0001",
+        Balance:   0,
+        Currency:  currency,
+        Color:     color,
+        Name:      name,
+        Active:    true,
+    }
+}
